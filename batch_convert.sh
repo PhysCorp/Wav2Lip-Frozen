d44902134a73690df45b87abdd23fc67d8db9ed7
Matt Curtis <physicallyrendered@gmail.com>
2023-01-25 09:33:14 -0500

    Create batch_convert.sh

---
+++ batch_convert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Read number of segments
+n_segments=$(cat "/content/input/length.txt")
+
+# Run inference for each segment
+for i in $(seq 0 $((n_segments-1))); do
+    python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "/content/input/$i/input_video.mp4" --audio "/content/input/$i/input_audio.wav"
+    mv /content/Wav2Lip/results/result_voice.mp4 /content/output/segment$i.mp4
+done
