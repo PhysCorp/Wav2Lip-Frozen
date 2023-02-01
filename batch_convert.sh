456a38f5022edba4092041305b3e322bf2e66a9d
Matt Curtis <physicallyrendered@gmail.com>
2023-01-31 21:06:23 -0500

    Update batch_convert.sh

--- batch_convert.sh
+++ batch_convert.sh
@@ -5,6 +5,6 @@ n_segments=$(cat "/content/input/length.txt")
 
 # Run inference for each segment
 for i in $(seq 0 $((n_segments-1))); do
-    python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "/content/input/$i/input_video.mp4" --audio "/content/input/$i/input_audio.wav"
+    python inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face "/content/input/$i/input_video.mp4" --audio "/content/input/$i/input_audio.wav" --pads 0 20 0 0
     mv /content/Wav2Lip/results/result_voice.mp4 /content/output/segment$i.mp4
 done
